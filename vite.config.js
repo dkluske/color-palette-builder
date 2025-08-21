f5f5c4291c99197c05ae42d12cbb76e0fc3b8d09
Dominik Kluske <dome.k28@yahoo.com>
2025-08-21 19:33:47 +0200

    no point as prefix in base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './color-palette-builder',
+  base: '/color-palette-builder',
   plugins: [react()],
 })
